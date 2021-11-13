4a47d555bd17cf62eed3c13f34a86a67cf5264d8
theArchitecturer <63711708+theArchitecturer@users.noreply.github.com>
2021-11-13 11:58:08 +0700

    Update first_setup.sh

--- first_setup.sh
+++ first_setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install --lts
 
 if [ "$1" = "ub" ]; then 
     sudo apt install python3-pip nodejs xclip 
